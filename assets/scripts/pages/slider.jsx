30d22f270687a0ee15bb7d0f541c232d6f4d9e3f
Chris Greeff <chris.r.greeff@gmail.com>
2017-05-03 15:41:05 +1200

    Adding labels to slider

--- assets/scripts/pages/slider.jsx
+++ assets/scripts/pages/slider.jsx
@@ -4,15 +4,38 @@ import React from 'react'
 import IpSection from '../components/section.jsx'
 
 export default class IpSlider extends React.Component {
-  constructor (props) {
-    super(props)
+  componentDidMount () {
+    $('.ip-slider').slider({
+      max: 1000,
+      values: [0, 300, 700, 1000]
+    }).on('slidechange', (e, ui) => {
+      console.log(e)
+      console.log(ui)
+    })
+
+    $('.ui-slider-handle').each((index, el) => {
+      const node = $(el)
+      let copy
+
+      if (index === 0) {
+        copy = 'Working'
+      } else if (index === 1) {
+        copy = 'Semi Retired'
+      } else if (index === 2) {
+        copy = 'Active Retirement'
+      } else {
+        copy = 'Old Age'
+      }
+
+      node.append(`<div class="ip-slider-handle-label">${copy}</div>`)
+    })
   }
 
   render () {
     return (
       <div className='ip-full-h ip-relative'>
         <IpSection>
-          
+          <div className='ip-slider' />
         </IpSection>
       </div>
     )
